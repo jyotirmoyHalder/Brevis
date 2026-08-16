bbde092bd9a1d85af66c69f77b567a33238dc97d
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2026-08-16 13:57:49 +0600

    Initial Commit

---
+++ Brevis/BrevisApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrevisApp.swift
+//  Brevis
+//
+//  Created by jyotirmoy_halder on 16/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BrevisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brevis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brevis
+//
+//  Created by jyotirmoy_halder on 16/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
